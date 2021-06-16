7fe28567f620634d1a3e3c2f18ab199c10add256
Stu Behan <33100049+StuBehan@users.noreply.github.com>
2021-06-16 14:30:47 +0100

    refactored code to accept any number of 1£s

--- src/changeConverter.js
+++ src/changeConverter.js
@@ -1,10 +1,18 @@
 class ChangeGenerator {
 
   convert(value) {
-    if (value === 1) {
-      return ['£1']
-    } else if (value === 2) {
-      return ['£1', '£1']
+    let change = []
+
+    if (value % 1 === 0) {
+      for (let i = 0; i < value /1; i++)
+      change.push('£1')
     }
+
+    return change
+    // if (value === 1) {
+    //   return ['£1']
+    // } else if (value === 2) {
+    //   return ['£1', '£1']
+    // }
   }
 }
