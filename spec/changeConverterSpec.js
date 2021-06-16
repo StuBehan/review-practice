46e24dabc6151624e26d7147864719ce81cf1e53
Stu Behan <33100049+StuBehan@users.noreply.github.com>
2021-06-16 14:22:57 +0100

    added response for value of 2

--- spec/changeConverterSpec.js
+++ spec/changeConverterSpec.js
@@ -5,4 +5,9 @@ describe('changeGenerator', () => {
     changeGenerator = new ChangeGenerator
     expect(changeGenerator.convert(1)).toEqual(['£1'])
   })
+
+  it('returns ["£1", "£1"] when passed 2', () => {
+    changeGenerator = new ChangeGenerator
+    expect(changeGenerator.convert(2)).toEqual(['£1', '£1'])
+  })
 })
--- src/changeConverter.js
+++ src/changeConverter.js
@@ -1,6 +1,10 @@
 class ChangeGenerator {
 
-  convert() {
-    return ['£1']
+  convert(value) {
+    if (value === 1) {
+      return ['£1']
+    } else if (value === 2) {
+      return ['£1', '£1']
+    }
   }
 }
