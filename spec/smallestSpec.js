2157ce886d0708f7b3b6c9f8834f33333b08e34e
Stu Behan <33100049+StuBehan@users.noreply.github.com>
2021-06-24 14:51:48 +0100

    passed test returning index of lowest single digit

--- spec/smallestSpec.js
+++ spec/smallestSpec.js
@@ -1,9 +1,9 @@
 describe('smallest', () => {
-  it('returns an array', () => {
-    expect(smallest()).toBeInstanceOf(Array)
+  it('returns a number within an array from the given number', () => {
+    expect (smallest(13)[0]).toEqual(13)
   })
 
-  it('returns a number within an array from the given number', () => {
-    expect (smallest(13)).toEqual([13])
+  it('returns the index of the lowest single digit in the number', () => {
+    expect(smallest(13)).toEqual([13, 0])
   })
 })
--- src/smallest.js
+++ src/smallest.js
@@ -1,4 +1,9 @@
 const smallest = (n) => {
+  const number = n.toString().split('')
 
-  return [n]
+  const lowestNum = Math.min(...number)
+
+  const j = number.indexOf(lowestNum.toString())
+
+  return [n, j]
 }
