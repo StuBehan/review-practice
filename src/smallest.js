573ace2a9c0c86727f0268e7bca7c99a536687fe
Stu Behan <33100049+StuBehan@users.noreply.github.com>
2021-06-24 15:24:36 +0100

    change of variable number to given argument array for function indexOfLowest

--- src/smallest.js
+++ src/smallest.js
@@ -2,7 +2,7 @@ const smallest = (n) => {
   const number = n.toString().split('')
 
   const indexOfLowest = (array) => {
-    return number.indexOf(Math.min(...array).toString())
+    return array.indexOf(Math.min(...array).toString())
   }
   
   return [n, indexOfLowest(number)]
