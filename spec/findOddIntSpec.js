d6290b4d30cd53bfef0cfed0be4641696bac26a6
Stu Behan <33100049+StuBehan@users.noreply.github.com>
2021-06-25 11:07:12 +0100

    passed for example test from Kata

--- spec/findOddIntSpec.js
+++ spec/findOddIntSpec.js
@@ -10,4 +10,8 @@ describe('findOdd', () => {
   it('returns 2 when passed the array [1, 1, 2]', () => {
     expect(findOdd([1, 1, 2])).toEqual(2)
   })
+
+  it('returns 5 when passed the array [20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]', () => {
+    expect(findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])).toEqual(5)
+  })
 })
--- src/findOddInt.js
+++ src/findOddInt.js
@@ -1,7 +1,6 @@
 const findOdd = (array) => {
   let result = 0
-
-  const f
+  
   array.forEach(element => {
     if(array.map(int => int === element).length % 2 !== 0) {
       result = element
