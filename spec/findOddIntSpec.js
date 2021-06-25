59b9ab8f842ec2ae271efa51e0fb2365b5a9bf30
Stu Behan <33100049+StuBehan@users.noreply.github.com>
2021-06-25 11:02:45 +0100

    passed test for finding most occuring odd entry in 3 number array

--- spec/findOddIntSpec.js
+++ spec/findOddIntSpec.js
@@ -2,4 +2,12 @@ describe('findOdd', () => {
   it('when given an array containing one int returns that int', () => {
     expect(findOdd([10])).toEqual(10)
   })
+
+  it('when passed an array only containing 5, returns 5', () => {
+    expect(findOdd([5])).toEqual(5)
+  })
+
+  it('returns 2 when passed the array [1, 1, 2]', () => {
+    expect(findOdd([1, 1, 2])).toEqual(2)
+  })
 })
--- src/findOddInt.js
+++ src/findOddInt.js
@@ -1,3 +1,12 @@
 const findOdd = (array) => {
-  return 10
+  let result = 0
+
+  const f
+  array.forEach(element => {
+    if(array.map(int => int === element).length % 2 !== 0) {
+      result = element
+    }
+  })
+
+  return result 
 }
